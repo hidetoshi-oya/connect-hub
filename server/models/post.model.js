02b42aa272c9371def978ef2dfdff8e970761def
大家英寿 <47383096+hidetoshi-oya@users.noreply.github.com>
2025-03-05 02:49:23 +0900

    Add post model

---
+++ server/models/post.model.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define("posts", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    isPinned: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    authorId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    views: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    }
+  }, {
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    paranoid: true, // 論理削除を有効化
+    deletedAt: 'deleted_at'
+  });
+
+  return Post;
+};
