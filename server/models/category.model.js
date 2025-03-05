7643a2184c816f7c0da42c9866283242af9f585e
大家英寿 <47383096+hidetoshi-oya@users.noreply.github.com>
2025-03-05 09:11:43 +0900

    category.model.jsファイルを追加

---
+++ server/models/category.model.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define("categories", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    is_active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
+  }, {
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  });
+
+  return Category;
+};
